2fb95c00f760e3e7dfcf7292146b35f5f2f85761
Adil Naseem <adilnaseemsheikh@gmail.com>
2023-10-08 12:23:56 +0500

    supabase configured

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ufarhfwnvrkiaxjhjrnl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmYXJoZndudnJraWF4amhqcm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3NDU3ODAsImV4cCI6MjAxMjMyMTc4MH0.rcL1Two00xIBM4SkYVBJIxxerK0m0yGOYqIeVZ87S4g";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
